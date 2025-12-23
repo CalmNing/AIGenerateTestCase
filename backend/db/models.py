9f15edb56bd63748a1b96660ca2615cc26308425
安宁 <anning96@163.com>
2025-12-23 16:22:59 +0800

    fix: 1. 修改创建时间和更新时间 2. docker-compose.yml 新增卷

--- backend/db/models.py
+++ backend/db/models.py
@@ -21,8 +21,8 @@ class StatusValue(str, Enum):
 
 class BaseModel(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow, sa_column_kwargs={"onupdate": datetime.utcnow})
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now, sa_column_kwargs={"onupdate": datetime.now})
 
 
 class Session(BaseModel, table=True):
